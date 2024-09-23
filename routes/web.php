bb9f5c06ee780eb5ac347723d8bb3c4929cc9e77
yuqi <yuqi.akagi@gmail.com>
2024-09-23 17:02:02 +0900

    booksテーブルとtypesテーブルのモデルとシーダーを作成。検索画面のビューの作成

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use \App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/top",[BookController::class, "index"])->name("top");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
